4d56939415c50d40f12722a30d1bfa0bd114f357
markozika <marko.zika@stud.th-deg.de>
2024-05-30 11:44:03 +0200

    Add code to GitHub

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, {createElement} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {/*
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>{!data ? "Loading..." : data}</p>
+        </header>
+      </div>
+  );*/
+}
+
+function sendMessage() {
+    let message = document.getElementById("message").value;
+    if (message.length !== 0) {
+        const node = document.createElement("h4");
+        node.setAttribute("id", "sent-message")
+        const textnode = document.createTextNode(message);
+        node.appendChild(textnode);
+        document.getElementById("messages-sent").appendChild(node);
+        let messagesSent = document.getElementById("messages-sent");
+        messagesSent.scrollTop = messagesSent.scrollHeight;
+    }
+}
+
+const messageButton = document.getElementById("send-button");
+
+messageButton.addEventListener("click", function () {
+    sendMessage();
+});
+
+var inputField = document.getElementById("message");
+inputField.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("send-button").click();
+        inputField.value = "";
+    }
+})
+export default App;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+﻿// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
